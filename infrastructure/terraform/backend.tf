a4990a7c4c4d642b23fd58f3e8e332bba1d3f193
seanpavlak <seanpavlak@gmail.com>
2026-01-25 16:46:32 -0500

    tf and output update

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,7 +1,7 @@
 # Terraform Backend Configuration (recreated after full reset)
 terraform {
   backend "s3" {
-    bucket         = "rearc-data-pipeline-terraform-state-1f66f7c0"
+    bucket         = "rearc-data-pipeline-terraform-state-788c706e"
     key            = "rearc-data-pipeline/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
